75bbd1df739ebeb3a1148b09daf95e900766cab8
Aybar <you@example.com>
2023-11-16 12:14:48 +0300

    first commit

--- src/test/java/day01/P02_DriverMethodlari.java
+++ src/test/java/day01/P02_DriverMethodlari.java
@@ -3,13 +3,15 @@ package day01;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class P01 {
+public class P02_DriverMethodlari {
     public static void main(String[] args) {
 
         System.setProperty("Wedriver.chrome.driver","src/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        driver.get("http://www.google.com ");
+        driver.get("http://www.wisequarter.com ");
+
+        driver.quit();
     }
 }
