17a701d3e35ae4fcbb17455f3158655161bb5bff
Aybar <you@example.com>
2023-11-16 13:44:34 +0300

    first commit

--- src/test/java/day01/P03_GetNavigate.java
+++ src/test/java/day01/P03_GetNavigate.java
@@ -1,8 +1,7 @@
+package day01;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import javax.annotation.concurrent.ThreadSafe;
-import java.time.Duration;
 
 public class P03_GetNavigate {
 
