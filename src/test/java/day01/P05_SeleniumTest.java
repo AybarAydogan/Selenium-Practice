290f032e8c0f750a8cd4095b2ee3c95d0b775911
Aybar <you@example.com>
2023-11-16 14:27:07 +0300

    first commit

--- src/test/java/day01/P05_SeleniumTest.java
+++ src/test/java/day01/P05_SeleniumTest.java
@@ -1,5 +1,6 @@
 package day01;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -18,12 +19,32 @@ Get the result
 Print the result
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("Wedriver.chrome.driver", "src/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10000));
+
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("calculatetest")).click();
+        // driver.findElement(By.xpath("//a[@id='calculatetest']")).click();
+        driver.findElement(By.id("number1")).sendKeys("136");
+        driver.findElement(By.xpath("//input[@id='number2']")).sendKeys("264");
+        driver.findElement(By.id("calculate")).click();
+        System.out.println("İşlem Sonuçu + " + driver.findElement(By.id("answer")).getText());
+
+        /*
+          WebElement answer = driver.findElement(By.id("answer"));
+         System.out.println("İşlem sonuçu = " + answer.getText());
+
+         */
+
+        driver.quit();
+
+
     }
 }
